485a25133dda4bd1f644a783ccb24cc523df19ca
UdaraWanasinghe <udara.developer@gmail.com>
2020-08-29 23:29:12 +0530

    add keyboard bindings

--- main.py
+++ main.py
@@ -1,115 +1,207 @@
 import vlc
 from tkinter import *
+from tkinter import filedialog
 
 
 class VideoTagger:
     def __init__(self):
-        self.root_window = Tk()
-        self.root_window.title('Video Tagger')
-        self.root_window.wm_attributes("-topmost", 1)
-        self.root_window.geometry("400x300")
-        self.media_player = None
-        self.is_playing = False
+        self._init_root_window()
+        self._init_media_player()
+        self._bind_keyboard_events()
         main_pane = PanedWindow(self.root_window, orient=VERTICAL)
-        self.url_input_entry = self.build_load_bar(main_pane)
-        self.play_button = self.build_control_bar(main_pane)
-        self.build_build_label_menu(main_pane)
-        self.build_save_button(main_pane)
+        browse_media_pane = self._build_browse_media_pane(main_pane)
+        media_control_pane = self._build_media_control_pane(main_pane)
+        label_drop_down_menu = self._build_label_dropdown_menu(main_pane)
+        save_button = self._build_save_button(main_pane)
+        add_button = self._build_add_button(main_pane)
+        delete_button = self._build_delete_button(main_pane)
+        label_list = self._build_label_list(main_pane)
+        main_pane.add(browse_media_pane)
+        main_pane.add(media_control_pane)
+        main_pane.add(label_drop_down_menu)
+        main_pane.add(add_button)
+        main_pane.add(delete_button)
+        main_pane.add(label_list)
+        main_pane.add(save_button)
         main_pane.pack()
         self.root_window.mainloop()
 
-    def build_load_bar(self, master):
-        paned_window = PanedWindow(master, orient=HORIZONTAL)
-        url_input_entry = Entry(paned_window)
-        load_button = Button(paned_window, text="Load", command=self.build_load_media_callback())
-        paned_window.add(url_input_entry)
-        paned_window.add(load_button)
-        master.add(paned_window)
-        return url_input_entry
+    def _init_root_window(self):
+        self.root_window = Tk()
+        self.root_window.title('Video Annotator')
+        self.root_window.wm_attributes("-topmost", 1)
+        self.root_window.geometry("400x400")
 
-    def build_control_bar(self, master):
-        paned_window = PanedWindow(master, orient=HORIZONTAL)
-        play_button = Button(paned_window, text="Play", command=self.build_play_button_callback())
-        forward_button = Button(paned_window, text=">", command=self.build_forward_button_callback())
-        fast_forward_button = Button(paned_window, text=">>", command=self.build_fast_forward_button_callback())
-        backward_button = Button(paned_window, text="<", command=self.build_backward_button_callback())
-        fast_backward_button = Button(paned_window, text="<<", command=self.build_fast_backward_button_callback())
-        paned_window.add(fast_backward_button)
-        paned_window.add(backward_button)
-        paned_window.add(play_button)
-        paned_window.add(forward_button)
-        paned_window.add(fast_forward_button)
-        master.add(paned_window)
-        return play_button
+    def _init_media_player(self):
+        self.media_player = vlc.MediaPlayer()
+        self.is_playing = False
+        events = self.media_player.event_manager()
+        events.event_attach(vlc.EventType.MediaPlayerPaused, self._on_paused)
+        events.event_attach(vlc.EventType.MediaPlayerPlaying, self._on_playing)
+        events.event_attach(vlc.EventType.MediaPlayerStopped, self._on_paused)
+        events.event_attach(vlc.EventType.MediaListEndReached, self._on_paused)
 
-    def build_build_label_menu(self, master):
-        string_var = StringVar(self.root_window)
-        option_menu = OptionMenu(master, string_var, *{'Option1', 'Option2'})
-        master.add(option_menu)
+    def _bind_keyboard_events(self):
+        root = self.root_window
 
-    def build_save_button(self, master):
-        save_button = Button(master, text='Save')
-        master.add(save_button)
+        def play_callback(e):
+            self._play_button_callback()
 
-    def build_load_media_callback(self):
-        def paused(event):
-            self.media_player.is_playing = False
-            self.play_button['text'] = 'Play'
+        def forward_callback(e):
+            self._fast_forward_button_callback()
 
-        def playing(event):
-            self.media_player.is_playing = True
-            self.play_button['text'] = 'Pause'
+        def fast_forward_callback(e):
+            self._fast_forward_button_callback()
 
-        def callback():
-            if self.media_player is not None:
-                self.media_player.release()
-            self.media_player = vlc.MediaPlayer(self.url_input_entry.get())
-            self.play_button['text'] = 'Play'
-            events = self.media_player.event_manager()
-            events.event_attach(vlc.EventType.MediaPlayerPaused, paused)
-            events.event_attach(vlc.EventType.MediaPlayerPlaying, playing)
-            events.event_attach(vlc.EventType.MediaPlayerStopped, paused)
+        def backward_callback(e):
+            self._backward_button_callback()
+
+        def fast_backward_callback(e):
+            self._fast_backward_button_callback()
+
+        def browse_callback(e):
+            self._build_load_media_callback()
+
+        def add_callback(e):
+            self._on_add_callback()
+
+        def delete_callback(e):
+            self._on_delete_callback()
+
+        def save_callback(e):
+            self._on_save_callback()
+
+        def print_e(e):
+            key_code = e.keycode
+            if 9 < key_code < 20 or 23 < key_code < 34 or 37 < key_code < 47 or 51 < key_code < 59:
+                code = -1
+                if 9 < key_code < 20:
+                    code = key_code - 10
+                elif 23 < key_code < 34:
+                    code = key_code - 14
+                elif 37 < key_code < 47:
+                    code = key_code - 18
+                elif 51 < key_code < 59:
+                    code = key_code - 23
+                if -1 < code < len(self.drop_down_labels):
+                    self.drop_down_value.set(self.drop_down_labels[code])
+            print(e)
+
+        root.bind("<space>", play_callback)
+        root.bind("<Right>", forward_callback)
+        root.bind("<Left>", backward_callback)
+        root.bind("<Up>", fast_forward_callback)
+        root.bind("<Down>", fast_backward_callback)
+        root.bind("<B>", browse_callback)
+        root.bind("<D>", delete_callback)
+        root.bind("<A>", add_callback)
+        root.bind("<S>", save_callback)
+        root.bind("<KeyPress>", print_e)
+
+    def _build_browse_media_pane(self, master):
+        pane = PanedWindow(master, orient=HORIZONTAL)
+        self.url_input_entry = Entry(pane)
+        self.browse_button = Button(pane, text="Browse", command=self._build_load_media_callback)
+        pane.add(self.url_input_entry)
+        pane.add(self.browse_button)
+        return pane
+
+    def _build_media_control_pane(self, master):
+        pane = PanedWindow(master, orient=HORIZONTAL)
+        self.play_button = Button(pane, text="Play", command=self._play_button_callback)
+        forward_button = Button(pane, text=">", command=self._forward_button_callback)
+        fast_forward_button = Button(pane, text=">>", command=self._fast_forward_button_callback)
+        backward_button = Button(pane, text="<", command=self._backward_button_callback)
+        fast_backward_button = Button(pane, text="<<", command=self._fast_backward_button_callback)
+        pane.add(fast_backward_button)
+        pane.add(backward_button)
+        pane.add(self.play_button)
+        pane.add(forward_button)
+        pane.add(fast_forward_button)
+        return pane
+
+    def _build_label_dropdown_menu(self, master):
+        self.drop_down_value = StringVar(self.root_window)
+        self.drop_down_labels = self._load_option_list()
+        self.drop_down_menu = OptionMenu(master, self.drop_down_value, *self.drop_down_labels)
+        self.drop_down_value.set(self.drop_down_labels[0])
+        return self.drop_down_menu
+
+    def _build_save_button(self, master):
+        return Button(master, text='Save', command=self._on_save_callback)
+
+    def _build_delete_button(self, master):
+        return Button(master, text='Delete', command=self._on_delete_callback)
+
+    def _build_add_button(self, master):
+        return Button(master, text='Add', command=self._on_add_callback)
+
+    def _build_label_list(self, master):
+        self.listbox = Listbox(master, height=10)
+        self.listbox.insert(1, '1')
+        self.listbox.insert(2, '2')
+        self.listbox.insert(3, '3')
+        return self.listbox
+
+    @staticmethod
+    def _load_option_list():
+        lines = []
+        with open('option_list.txt') as file:
+            for line in file:
+                line = line.strip()
+                lines.append(line)
+        return lines
+
+    def _build_load_media_callback(self):
+        filename = filedialog.askopenfilename(initialdir="./",
+                                              title="Select a Video")
+        if len(filename) > 0:
+            self.url_input_entry.delete(0, 'end')
+            self.url_input_entry.insert(0, filename)
+            media = vlc.Media(filename)
+            self.media_player.set_media(media)
+            self.media_player.play()
+            self.annotations = {}
+
+    def _play_button_callback(self):
+        if self.is_playing:
+            self.media_player.pause()
+        else:
             self.media_player.play()
 
-        return callback
+    def _forward_button_callback(self):
+        u_time = self.media_player.get_time() + 5000
+        self.media_player.set_time(u_time)
 
-    def build_play_button_callback(self):
-        def play_button_callback():
-            if self.is_playing:
-                self.media_player.pause()
-                self.play_button['text'] = "Play"
-            else:
-                self.media_player.play()
-                self.play_button['text'] = "Pause"
-            self.is_playing = not self.is_playing
+    def _backward_button_callback(self):
+        u_time = self.media_player.get_time() - 5000
+        self.media_player.set_time(u_time)
 
-        return play_button_callback
+    def _fast_forward_button_callback(self):
+        self.media_player.set_rate(self.media_player.get_rate() + 1)
 
-    def build_forward_button_callback(self):
-        def callback():
-            u_time = self.media_player.get_time() + 5000
-            self.media_player.set_time(u_time)
+    def _fast_backward_button_callback(self):
+        self.media_player.set_rate(self.media_player.get_rate() - 1)
 
-        return callback
+    def _on_paused(self, _):
+        self.is_playing = False
+        self.play_button['text'] = 'Play'
 
-    def build_backward_button_callback(self):
-        def callback():
-            u_time = self.media_player.get_time() - 5000
-            self.media_player.set_time(u_time)
+    def _on_playing(self, _):
+        self.is_playing = True
+        self.play_button['text'] = 'Pause'
 
-        return callback
+    def _on_end_reached(self, _):
+        self.media_player.set_time(0)
 
-    def build_fast_forward_button_callback(self):
-        def callback():
-            self.media_player.set_rate(self.media_player.get_rate() + 1)
+    def _on_add_callback(self):
+        print(self)
 
-        return callback
+    def _on_save_callback(self):
+        print(self)
 
-    def build_fast_backward_button_callback(self):
-        def callback():
-            self.media_player.set_rate(self.media_player.get_rate() - 1)
-
-        return callback
+    def _on_delete_callback(self):
+        print(self)
 
 
 videoTagger = VideoTagger()
